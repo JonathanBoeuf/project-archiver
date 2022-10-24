40715f6c8cd69d21a88dc364dff01e66a1a76d71
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-24 15:56:26 +0000

    Adding tarfile member sanitization to extractall()

--- RestoreArchive.py
+++ RestoreArchive.py
@@ -131,7 +131,26 @@ if __name__ == '__main__':
             sys.exit(1)
 
     with tarfile.open(output_file, "r") as basic_tar:
-        basic_tar.extractall(args.output_folder)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(basic_tar, args.output_folder)
         extracted_folder = os.path.join(args.output_folder, os.path.commonprefix(basic_tar.getnames()))
         logging.info("Intermediate tarfile %s has been extracted into %s, and will now be checked" %
                    (output_file, extracted_folder), args.alsologtostderr)
